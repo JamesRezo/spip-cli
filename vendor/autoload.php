24c64d4e72978590063a731fdf03b34a0da2a567
bystrano@gmx.ch <>
2014-11-04 18:50:35 +0000

    mise à jour vers Symfony 2.5

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit588c86bee81edc0edb0115f1c0860f86::getLoader();
+return ComposerAutoloaderInitee28b652c654b96388f0aeca8b91c1f9::getLoader();

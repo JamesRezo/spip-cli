3e16c6a762bde150304d173475a895b16889c103
bystrano@gmx.ch <>
2015-10-13 17:41:27 +0000

    Ajoute une commande pour convertir du texte au format SPIP en HTML
    via la fonction "propre".
    
    On convertit ce qui arrive dans STDIN en html,
    par exemple :
    
        echo "{{{ hello }}}" | spip texte:propre
    
    retourne :
    
        <h3 class="spip"> hello </h3>

---
+++ spip-cli/TextePropre.php
@@ -0,0 +1,34 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TextePropre extends Command {
+	protected function configure() {
+		$this
+			->setName('texte:propre')
+			->setDescription('Converti du texte au format SPIP vers du HTML via la fonction "propre"')
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		global $spip_racine;
+		global $spip_loaded;
+
+		if ($spip_loaded) {
+			chdir($spip_racine);
+
+			$contenu = stream_get_contents(STDIN);
+
+			include_spip('inc/texte');
+			$output->write(propre($contenu));
+
+		}
+		else{
+			$output->writeln('<error>Vous n’êtes pas dans une installation de SPIP. Impossible de convertir le texte.</error>');
+		}
+	}
+}

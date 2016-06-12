3eaf02145f860541cdcf8d34259d19ea4ac1761c
cam.lafit@azerttyu.net <>
2016-06-12 17:03:15 +0000

    Activer un depot en CLI
    * Fournir l'url d'un depot pour l'activer
    * Attention en l'état aucun contrôle sur l'url
    Exemple :
    @spip plugins:depoter http://plugins.spip.net/depots/principal.xml@

---
+++ spip-cli/PluginsDepoter.php
@@ -0,0 +1,47 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class PluginsDepoter extends Command {
+    protected function configure() {
+        $this
+            ->setName('plugins:depoter')
+            ->setDescription('Ajouter un depot')
+            ->addArgument(
+                'url',
+                InputArgument::REQUIRED,
+                'URL du dépot'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        global $spip_loaded;
+        global $spip_racine;
+        global $cwd;
+
+        if ($spip_loaded) {
+            chdir($spip_racine);
+
+            include_spip('inc/svp_depoter_distant');
+            $url_depot = $input->getArgument('url');
+
+            $ajouter= svp_ajouter_depot($url_depot);
+
+            if ( ! $ajouter) {
+                $output->writeln("<error>Impossible d'ajouter le dépot $url</error>");
+            } else {
+                $output->writeln("<info>Le dépot $url a été ajouté</info>");
+            }
+
+            chdir($cwd);
+        }
+    }
+}

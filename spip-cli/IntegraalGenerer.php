d061cc4c9229344c385072d7c442bac7327d2342
tcharlss@bravecassine.com <>
2017-11-20 23:43:59 +0000

    Ajustements pour la génération d'integraal : on peut choisir la version du thème avec l'option -t : gulp (défaut) ou scssphp

--- spip-cli/IntegraalGenerer.php
+++ spip-cli/IntegraalGenerer.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 /**
  * Générer un nouveau projet à partir de l'échafaudage Intégraal
@@ -46,6 +47,13 @@ class IntegraalGenerer extends Command {
 				InputOption::VALUE_REQUIRED,
 				'URL de l’auteur du projet'
 			)
+			->addOption(
+				'theme',
+				't',
+				InputOption::VALUE_REQUIRED,
+				'Version du thème à utiliser',
+				'gulp'
+			)
 		;
 	}
 
@@ -57,9 +65,24 @@ class IntegraalGenerer extends Command {
 		}
 		
 		// On récupère les options
-		$chemin_integraal = $input->getOption('chemin_integraal');
+		$chemin_integraal = rtrim($input->getOption('chemin_integraal'), '/') . '/';
 		$auteur = $input->getOption('auteur');
 		$url = $input->getOption('url');
+		$version_theme = $input->getOption('theme');
+		
+		// [TODO] Si des arguments nécessaires manquent, on pose des questions
+		/*
+		if (!$version_theme) {
+			$helper = $this->getHelper('question');
+			$question_theme = new ChoiceQuestion(
+				'Quelle version du thème utiliser ? (défaut : gulp)',
+				array('gulp', 'scssphp'),
+				0
+			);
+			$version_theme = $helper->ask($input, $output, $question_theme);
+			$output->writeln('Vous avez choisi : '.$version_theme);
+		}
+		*/
 		
 		// On se déplace dans le dossier des plugins si on est dans un SPIP
 		if (
@@ -78,39 +101,75 @@ class IntegraalGenerer extends Command {
 			// On télécharge Intégraal
 			$output->writeln("<info>Téléchargement de l’échafaudage IntéGraal.</info>");
 			
-			// On lance la commande SVN dans le répertoire courant
-			passthru("svn export {$chemin_integraal} {$prefixe}", $erreur);
-			
-			if ($erreur !== 0) {
-				$output->writeln("<error>Une erreur s’est produite durant le téléchargement.</error>");
+			// On créé un nouveau répertoire pour y accueillir les 3 plugins d'Intégraal
+			if (!$dir_integraal = mkdir($prefixe, 0775)) {
+				$output->writeln("<error>Le répertoire « $prefixe » n’a pas pu être créé dans le dossier des plugins.</error>");
 			} else {
-				passthru("chmod 775 -R {$prefixe}");
 				
 				// On se déplace dans le dossier du nouveau projet
 				chdir($prefixe);
 				
-				// On renomme tous les fichiers avec "integraal"
-				passthru("find . | rename -v 's/integraal/{$prefixe}/g'", $erreur);
+				// Retrouver le chemin correspondant à la version du thème choisie
+				$chemins_theme = array(
+					'gulp'    => 'theme/trunk',
+					'scssphp' => 'theme/branches/scssphp',
+				);
+				$chemin_theme = $chemins_theme[$version_theme];
 				
-				// On affiche une erreur mais on continue quand même le renommage des contenus ensuite
-				if ($erreur !== 0) {
-					$output->writeln("<error>Une erreur s’est produite durant le renommage des fichiers.</error>");
+				// On lance la commande SVN pour télécharger les 3 plugins
+				// dans le répertoire courant
+				$erreur_telecharger = false;
+				$sous_chemins_integraal = array(
+					'squelettes',
+					$chemin_theme,
+					'core'
+				);
+				foreach($sous_chemins_integraal as $dossier){
+					$chemin_sous_dossier = $chemin_integraal . $dossier;
+					passthru("svn export {$chemin_sous_dossier}", $erreur);
+					if ($erreur !== 0) {
+						$erreur_telecharger = true;
+						break;
+					}
 				}
 				
-				// On remplace à l'intérieur des contenus
-				passthru("find . -type f -exec sed -i 's/integraal/{$prefixe}/g' {} \;", $erreur);
-				passthru("find . -type f -exec sed -i 's/IntéGraal/{$nom}/g' {} \;", $erreur);
-				
-				// Si on demande à changer l'auteur
-				if ($auteur) {
-					passthru("find . -type f -exec sed -i 's/Les Développements Durables/{$auteur}/g' {} \;", $erreur);
+				if ($erreur_telecharger) {
+					$output->writeln("<error>Une erreur s’est produite durant le téléchargement.</error>");
+				} else {
+					
+					// Mettre les bonnes permissions
+					passthru("chmod 775 -R *");
+					
+					// On renomme le dossier du theme
+					$dossier_theme = ltrim(strrchr($chemin_theme, '/'), '/');
+					passthru("mv $dossier_theme theme", $erreur);
+					if ($erreur){
+						$output->writeln("<error>Le dossier du theme (${dossier_theme}) n’a pas pu être renommé en « theme »</error>");
+					}
+					
+					// On renomme tous les fichiers avec "integraal"
+					passthru("find . | rename -v 's/integraal/{$prefixe}/g'", $erreur);
+					
+					// On affiche une erreur mais on continue quand même le renommage des contenus ensuite
+					if ($erreur !== 0) {
+						$output->writeln("<error>Une erreur s’est produite durant le renommage des fichiers.</error>");
+					}
+					
+					// On remplace à l'intérieur des contenus
+					passthru("find . -type f -exec sed -i 's/integraal/{$prefixe}/g' {} \;", $erreur);
+					passthru("find . -type f -exec sed -i 's/IntéGraal/{$nom}/g' {} \;", $erreur);
+					
+					// Si on demande à changer l'auteur
+					if ($auteur) {
+						passthru("find . -type f -exec sed -i 's/Les Développements Durables/{$auteur}/g' {} \;", $erreur);
+					}
+					// Si on demande à changer l'url
+					if ($url) {
+						passthru("find . -type f -exec sed -i 's|http://www\.ldd\.fr|{$url}|g' {} \;", $erreur);
+					}
+					
+					$output->writeln('<info>C’est fini ! Vous pouvez maintenant personnaliser les fichiers selon vos besoins.</info>');
 				}
-				// Si on demande à changer l'auteur
-				if ($url) {
-					passthru("find . -type f -exec sed -i 's|http://www\.ldd\.fr|{$url}|g' {} \;", $erreur);
-				}
-				
-				$output->writeln('<info>C’est fini ! Vous pouvez maintenant personnaliser les fichiers selon vos besoins.</info>');
 			}
 		}
 	}

57fdb8f69429415b9e455222f71ba78cd6c69466
rastapopoulos@spip.org <>
2018-03-19 12:32:17 +0000

    Passage en Symfony 4 du composant Console. J'ai testé toutes les commandes de base par défaut, qui sont compatibles, vu qu'elles sont relativement basiques. Désormais il faut PHP 7 pour le lancer (le PHP en ligne de commande, pas obligatoirement le même que celui activé pour Apache).

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,28 +23,48 @@ class ComposerAutoloaderInitee28b652c654b96388f0aeca8b91c1f9
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitee28b652c654b96388f0aeca8b91c1f9', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInitee28b652c654b96388f0aeca8b91c1f9::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
+
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitee28b652c654b96388f0aeca8b91c1f9::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireee28b652c654b96388f0aeca8b91c1f9($fileIdentifier, $file);
+        }
 
         return $loader;
     }
 }
 
-function composerRequireee28b652c654b96388f0aeca8b91c1f9($file)
+function composerRequireee28b652c654b96388f0aeca8b91c1f9($fileIdentifier, $file)
 {
-    require $file;
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+    }
 }

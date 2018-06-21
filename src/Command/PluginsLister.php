662bc43c8bb656978c24658288a299217440d5ea
marcimat@rezo.net <>
2018-06-21 15:25:54 +0000

    Réécriture de "plugins:lister". On remet le code de Polatouche pour cette fonction.
    - N’affiche pas par défaut les plugins "procurés"
    - N’affiche pas par défaut les extensions PHP
    - Beaucoup d’options.
    
    Exemple :
    ```
    spip plugins:lister --short
    spip plugins:lister --no-dist
    spip plugins:lister --php
    spip plugins:lister --inactifs --short
    ```
    
    - Permet d’exporter la liste des préfixes des plugins actifs
    dans un fichier tmp/plugins.txt (genre avant une migation)
    (une commande à venir permettra de réactiver des plugins à partir de ce fichier)
    
    `spip plugins:lister --export`

--- src/Command/PluginsLister.php
+++ src/Command/PluginsLister.php
@@ -2,104 +2,215 @@
 
 namespace Spip\Cli\Command;
 
+use Spip\Cli\Console\Style\SpipCliStyle;
+use Spip\Cli\Loader\Spip;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Helper\Table;
+
 
 class PluginsLister extends Command {
-    protected function configure() {
-        $this
-            ->setName('plugins:lister')
-            ->setDescription('Lister les plugins activés.')
-            ->addOption(
-                'disponibles',
-                'd',
-                InputOption::VALUE_NONE,
-                'Montrer les plugins disponibles'
-            )
-            ->addOption(
-                'inactifs',
-                'i',
-                InputOption::VALUE_NONE,
-                'Montrer les plugins inactifs'
-            )
-        ;
-    }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        global $spip_loaded;
-        global $spip_racine;
-        global $cwd;
+	/** @var SpipCliStyle */
+	protected $io;
 
-        if ($spip_loaded) {
-            chdir($spip_racine);
+	protected function configure() {
+		$this->setName("plugins:lister")
+			->setDescription("Liste les plugins du site.")
+			->addOption('dist', null, InputOption::VALUE_NONE, 'Uniquement les plugins dist')
+			->addOption('no-dist', null, InputOption::VALUE_NONE, 'Exclure les plugins dist')
+			->addOption('procure', null, InputOption::VALUE_NONE, 'Uniquement les plugins procurés')
+			->addOption('php', null, InputOption::VALUE_NONE, 'Uniquement les extensions PHP présentes')
+			->addOption('spip', null, InputOption::VALUE_NONE, 'Uniquement SPIP')
+			->addOption('short', null, InputOption::VALUE_NONE, 'Affiche simplement le préfixe')
 
-            if ($input->getOption('disponibles')) {
+			->addOption('inactifs', null, InputOption::VALUE_NONE, 'Liste les plugins inactifs.')
 
-                $plugins = lister_plugins(
-                    'paquets.constante="_DIR_PLUGINS"'
-                );
+			->addOption('export', 'e', InputOption::VALUE_NONE, 'Exporter la liste des plugins actifs dans un fichier')
+			->addOption('name', null, InputOption::VALUE_OPTIONAL, 'Nom du fichier d’export', 'plugins')
+		;
+	}
 
-            } else if ($input->getOption('inactifs')) {
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$this->io = $this->getApplication()->getIO($input, $output);
 
-                $plugins = lister_plugins(array(
-                    'paquets.constante="_DIR_PLUGINS"',
-                    'paquets.actif!="oui"',
-                ));
+		/** @var Spip $spip */
+		$spip = $this->getApplication()->getService('spip.loader');
+		$spip->load();
+		$spip->chdir();
 
-            } else {
+		$this->io->title("Liste des plugins");
 
-                $plugins = unserialize($GLOBALS['meta']['plugin']);
+		$this->actualiserPlugins();
 
-            }
+		if ($input->getOption('inactifs')) {
+			$this->showInactifs($input);
+		} elseif ($input->getOption('export')) {
+			$this->exportActifs($input);
+		} else {
+			$this->showActifs($input);
+		}
+	}
 
-            include_spip('inc/filtres');
-            $data = array_map(function ($plugin) {
-                return array(
-                    trim(html_entity_decode(
-                        extraire_multi($plugin['nom'], 'fr'))),
-                    $plugin['etat'],
-                    joli_no_version($plugin['version']),
-                );
-            }, $plugins);
+	public function showActifs(InputInterface $input) {
+		$options = [
+			'dist' => null,
+			'procure' => false,
+			'php' => false,
+			'spip' => false,
+		];
+		$list = [];
 
-            $table = new Table($output);
-            $table->setHeaders(array('Nom', 'État', 'Version'));
-            $table->setRows($data);
-            $table->render();
+		if ($input->getOption('php')) {
+			$options['php'] = true;
+			$options['procure'] = true;
+			$list[] = 'Uniquement les extensions PHP procurées';
+		}
 
-            chdir($cwd);
+		if ($input->getOption('procure')) {
+			$options['procure'] = true;
+			$list[] = 'Uniquement les plugins procurés';
+		}
 
-        } else {
+		if ($input->getOption('dist')) {
+			$options['dist'] = true;
+			$list[] = 'Uniquement les plugins-dist';
+		} elseif ($input->getOption('no-dist')) {
+			$options['dist'] = false;
+			$list[] = 'Sans les plugins-dist';
+		}
 
-            $output->writeln("<comment>Vous n'êtes pas dans une installation de SPIP, il n'y a pas de plugins disponibles.</comment>");
+		if ($input->getOption('spip')) {
+			$options['spip'] = true;
+			$list[] = 'Uniquement SPIP';
+		}
 
-        }
-    }
-}
+		if ($list) {
+			$this->io->listing($list);
+		}
 
-function lister_plugins ($where) {
+		$actifs = $this->getPluginsActifs($options);
+		$this->showPlugins($actifs, $input->getOption('short'));
+	}
 
-    include_spip('base/abstract_sql');
-    $plugins = sql_allfetsel('plugins.nom, paquets.etat, paquets.version',
-                             'spip_plugins as plugins' .
-                             ' INNER JOIN spip_paquets as paquets' .
-                             ' ON plugins.prefixe=paquets.prefixe',
-                             $where);
+	public function showInactifs(InputInterface $input) {
+		$list = ["Liste des plugins inactifs"];
+		$this->io->listing($list);
+		$inactifs = $this->getPluginsInactifs();
+		$this->showPlugins($inactifs);
+	}
 
-    return $plugins;
-}
+	public function exportActifs(InputInterface $input) {
+		$name = $input->getOption('name') . '.txt';
+		$file = _DIR_TMP . $name;
 
-function joli_no_version ($moche) {
+		$actifs = $this->getPluginsActifs([
+			'procure' => false,
+			'php' => false,
+		]);
 
-    $tab_moche = explode('.', $moche);
+		$list = implode(" ", array_map('strtolower', array_keys($actifs)));
+		if (file_put_contents($file, $list)) {
+			$this->io->check("Export dans : " . $file);
+		} else {
+			$this->io->fail("Export raté : " . $file);
+		}
+		$this->io->text($list);
+		// $this->io->columns(explode(" ", $list), 6, true);
+	}
 
-    $tab_joli = array_map(function ($n) {
-        return ltrim($n,'0') ?: '0';
-    }, $tab_moche);
+	public function actualiserPlugins() {
+		include_spip('inc/plugin');
+		actualise_plugins_actifs();
+	}
 
-    return implode('.', $tab_joli);
+	/**
+	 * Obtenir la liste des plugins actifs
+	 *
+	 * @param array $options {
+	 *     @var bool|null $php Afficher|Exclure|Uniquement les extensions PHP
+	 *     @var bool $spip Afficher le faux plugin 'SPIP'
+	 *     @var bool|null $dist Afficher|Exclure|Uniquement les plugins dist
+	 * }
+	 * @return array
+	 */
+	public function getPluginsActifs(array $options = []) {
+		$options += [
+			'procure' => null,
+			'php' => null,
+			'spip' => false, // only SPIP ?
+			'dist' => null,
+		];
+		$plugins = unserialize($GLOBALS['meta']['plugin']);
+		if ($options['spip']) {
+			return ['SPIP' => $plugins['SPIP']];
+		} else {
+			unset($plugins['SPIP']);
+		}
+
+		foreach ($plugins as $k => $v) {
+			$is = [
+				'php' => ($k === 'PHP' or strpos($k, 'PHP:') === 0),
+				'dist' => ($v['dir_type'] === '_DIR_PLUGINS_DIST'),
+				'procure' => (strpos($v['dir'], 'procure:') !== false),
+			];
+			foreach ($is as $quoi => $test) {
+				if (!is_null($options[$quoi]) and ($options[$quoi] xor $is[$quoi])) {
+					unset($plugins[$k]);
+				}
+			}
+
+		}
+
+		return $plugins;
+	}
+
+	public function getPluginsInactifs() {
+		// chercher dans les plugins dispo
+		$get_infos = charger_fonction('get_infos','plugins');
+
+		$dirs = ['_DIR_PLUGINS' => _DIR_PLUGINS];
+		if (defined('_DIR_PLUGINS_SUPPL') and _DIR_PLUGINS_SUPPL) {
+			$dirs['_DIR_PLUGINS_SUPPL'] = _DIR_PLUGINS_SUPPL;
+		}
+
+		$list = [];
+		foreach($dirs as $const => $dp) {
+			$plugins = liste_plugin_files($dp);
+			foreach($plugins as $dir){
+				$infos = $get_infos($dir, false, $dp);
+				$prefix = strtoupper($infos['prefix']);
+				$list[] = [
+					'nom' => $prefix,
+					'etat' => $infos['etat'],
+					'version' => $infos['version'],
+					'dir' => $dir,
+					'dir_type' => $const,
+				];
+			}
+		}
+		return $list;
+	}
+
+	public function showPlugins(array $list, $short = false) {
+		ksort($list);
+		if ($short) {
+			$list = array_keys($list);
+			$list = array_map('strtolower', $list);
+			$list = array_map('ucfirst', $list);
+			$this->io->columns($list, 6, true);
+		} else {
+			$this->io->atable($list);
+		}
+	}
+
+
+	public function presenterListe(array $liste) {
+		if (count($liste) > 4) {
+			$this->io->columns($liste, 6, true);
+		} else {
+			$this->io->listing($liste);
+		}
+	}
 }

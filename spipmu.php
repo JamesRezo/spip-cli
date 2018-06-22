ac6576f41858b70b33b672d79667bdb3fea8e27b
real3t@gmail.com <>
2018-06-22 07:52:33 +0000

    Il manquait ce fichier pour pouvoir utiliser spipmu
    Et il faut en SSH avoir fait :
    sudo ln -s /opt/spip-cli/spipmu.php /usr/local/bin/spipmu
    pour disposer de l'appel en ligne de commande : spipmu

---
+++ spipmu.php
@@ -0,0 +1,5 @@
+#!/usr/bin/env php
+<?php
+// spip
+echo "\n[deprecated] Veuillez utiliser les executables SPIP-Cli du répertoire bin.\n";
+include_once(__DIR__ . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'spipmu');

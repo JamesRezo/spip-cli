7dd25b45b59bdc6eee706c7d6c6d5a6342496a4d
Matthieu Marcillaud <marcimat@rezo.net>
2020-06-09 12:30:44 +0200

    En fonction d’où est placé le plugin spip-cli, il pouvait croire que spip-cli/spip.php à l’intérieur de ce plugin était une commande à charger. (par exemple si on plaçait un répertoire spip-cli à la racine de SPIP pour que l’IDE connaisse ses fonctions).

--- src/Application.php
+++ src/Application.php
@@ -16,7 +16,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 class Application extends ConsoleApplication {
 
 	const NAME = "Spip Cli";
-	const VERSION = "0.5.4";
+	const VERSION = "0.5.5";
 
 	/** @var Container */
 	protected $container;
@@ -171,6 +171,10 @@ class Application extends ConsoleApplication {
 		$cwd = getcwd();
 		$spip->chdir();
 		$commandes = find_all_in_path('spip-cli/', '.*[.]php$');
+		// pas le spip.php à la racine de ce plugin (l’ancien exécutable pour démarrer spip-cli);
+		$commandes = array_filter($commandes, function($commande) {
+			return strpos($commande, "spip-cli/spip.php") === false;
+		});
 		foreach ($commandes as $path) {
 			$this->registerSpipCliCommand($path);
 		}
